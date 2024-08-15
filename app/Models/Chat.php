d89feb26a84c95165e0458d43a52fb04d2ea64c4
Saumik Chowdhury <47342920+saumiknadan@users.noreply.github.com>
2024-08-15 11:38:09 +0600

    table column include in model

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Chat extends Model
 {
     use HasFactory;
+    protected $fillable = ['from_user_id', 'to_user_id', 'chat_message', 'message_status'];
 }
--- app/Models/ChatRequest.php
+++ app/Models/ChatRequest.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class ChatRequest extends Model
 {
     use HasFactory;
+    protected $fillable = ['from_user_id', 'to_user_id', 'status'];
 }
