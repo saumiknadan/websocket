9eab07765ea944e374f8bc7a42addaaf66dead5b
Saumik Chowdhury <47342920+saumiknadan@users.noreply.github.com>
2024-08-15 16:04:46 +0600

    update user table

---
+++ database/migrations/2024_08_15_100222_update_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('token');
+            $table->integer('connection_id');
+            $table->enum('user_status', ['Offline', 'Online']);
+            $table->string('user_image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('token');
+            $table->dropColumn('connection_id');
+            $table->dropColumn('user_status');
+            $table->dropColumn('user_image');
+        });
+    }
+};
