d4f257d837aa14c3f73a6deb2a76b22e44c034bb
Saumik Chowdhury <47342920+saumiknadan@users.noreply.github.com>
2024-08-15 15:50:46 +0600

    write handle function in WebSocket Server

--- app/Console/Commands/WebSocketServer.php
+++ app/Console/Commands/WebSocketServer.php
@@ -4,6 +4,16 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
+use Ratchet\Server\IoServer;
+
+use Ratchet\Http\HttpServer;
+
+use Ratchet\WebSocket\WsServer;
+
+use React\EventLoop\Factory;
+
+use App\Http\Controllers\SocketController;
+
 class WebSocketServer extends Command
 {
     /**
@@ -25,6 +35,17 @@ class WebSocketServer extends Command
      */
     public function handle()
     {
-        //
+        //return 0;
+
+        $server = IoServer::factory(
+            new HttpServer(
+                new WsServer(
+                    new SocketController()
+                )
+            ),
+            8090
+        );
+
+        $server->run();
     }
 }
